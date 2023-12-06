9d0ca1993b0168adc71b995c3c0fdbdefe0a0283
ramiz_ramji <ramizramji98@gmail.com>
2023-12-06 20:06:47 +0530

    nfe error docs 3

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,53 +1,47 @@
 <div class="navbar-default sidebar" role="navigation" id="sidebar-component">
     <div class="sidebar-nav slimscrollsidebar">
         <div class="sidebar-head">
-            <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span>
-            </h3>
+            <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span></h3>
         </div>
         <ul class="nav" id="side-menu" style="margin-top: 60px">
             <li>
                 <a href="{{ url('dashboard') }}" class="waves-effect">
                     <i class="mdi mdi-home hideMenu" data-icon="v"></i>
-                    <span class="hide-menu hideMenu"> {{ __('menu.dashboard') }}</span>
+                    <span class="hide-menu hideMenu">{{ __('menu.dashboard') }}</span>
                 </a>
             </li>
+
             <?php
             $sideMenu = showMenu();
-            $menuItem = '';
-
             foreach ($sideMenu as $key => $value) {
-                $menuItem .= '<li class="treeview waves-effect"><a href="javascript:void(0)" class="module"><i class="iconFontSize ' . $value['icon_class'] . ' hideMenu"></i> <span class="hide-menu hideMenu">&nbsp;' . __('menu' . '.' . str_replace(' ', '_', strtolower($value['name']))) . '<span class="fa arrow"></span></span></a>';
+                echo '<li class="treeview waves-effect">';
+                echo '<a href="javascript:void(0)" class="module"><i class="iconFontSize ' . $value['icon_class'] . ' hideMenu"></i>';
+                echo '<span class="hide-menu hideMenu">&nbsp;' . __('menu' . '.' . str_replace(' ', '_', strtolower($value['name']))) . '<span class="fa arrow"></span></span></a>';
 
                 if ($value['sub_menu']) {
-                    $menuItem .= '<ul class="treeview-menu nav nav-second-level">';
-
+                    echo '<ul class="treeview-menu nav nav-second-level">';
                     foreach ($value['sub_menu'] as $menu) {
-                        if ($menu['menu_url'] != '' || $menu['sub_menu']) {
-                            $menuItem .= '<li><a href="' . ($menu['menu_url'] ? route($menu['menu_url']) : 'javascript:void(0)') . '"><i data-icon="/" class="linea-icon linea-basic fa-fw"></i><span class="hideMenu">' . __('menu' . '.' . str_replace(' ', '_', strtolower($menu['name']))) . '</span>' . ($menu['sub_menu'] ? '<i class="fa arrow"></i>' : '') . '</a>';
+                        echo '<li>';
+                        echo '<a href="' . ($menu['menu_url'] ? route($menu['menu_url']) : 'javascript:void(0)') . '">';
+                        echo '<i data-icon="/" class="linea-icon linea-basic fa-fw"></i>';
+                        echo '<span class="hideMenu">' . __('menu' . '.' . str_replace(' ', '_', strtolower($menu['name']))) . '</span>';
+                        echo ($menu['sub_menu'] ? '<i class="fa arrow"></i>' : '') . '</a>';
 
-                            if ($menu['sub_menu']) {
-                                $menuItem .= '<ul class="treeview-menu nav nav-third-level">';
-
-                                foreach ($menu['sub_menu'] as $subMenu) {
-                                    $menuItem .= '<li class=""><a class="hideMenu" href="' . ($subMenu['menu_url'] ? route($subMenu['menu_url']) : 'javascript:void(0)') . '"> <i class="fa fa-circle-o"></i> &nbsp;' . __('menu' . '.' . str_replace(' ', '_', strtolower($subMenu['name']))) . '</a></li>';
-                                }
-
-                                $menuItem .= '</ul>';
+                        if ($menu['sub_menu']) {
+                            echo '<ul class="treeview-menu nav nav-third-level">';
+                            foreach ($menu['sub_menu'] as $subMenu) {
+                                echo '<li class=""><a class="hideMenu" href="' . ($subMenu['menu_url'] ? route($subMenu['menu_url']) : 'javascript:void(0)') . '">';
+                                echo '<i class="fa fa-circle-o"></i> &nbsp;' . __('menu' . '.' . str_replace(' ', '_', strtolower($subMenu['name']))) . '</a></li>';
                             }
-
-                            $menuItem .= '</li>';
+                            echo '</ul>';
                         }
+                        echo '</li>';
                     }
-
-                    $menuItem .= '</ul>';
+                    echo '</ul>';
                 }
-
-                $menuItem .= '</li>';
+                echo '</li>';
             }
-
-            echo $menuItem;
             ?>
-
         </ul>
     </div>
 </div>
